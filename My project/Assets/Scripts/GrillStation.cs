e0f290cd8a3a1866afb0784fc7e09388e50551ad
Anson_Deck <anson.aw0618@gmail.com>
2024-01-27 23:48:23 +0000

    Anson: removed old grill code

--- My project/Assets/Scripts/GrillStation.cs
+++ My project/Assets/Scripts/GrillStation.cs
@@ -4,53 +4,53 @@ using UnityEngine;
 
 public class GrillStation : Station
 {
-    private float _timer;
+    // private float _timer;
 
     public override void OnFoodAdded(FoodItem food)
     {
-        base.OnFoodAdded(food);
-
-        _timer = 0;
+        // base.OnFoodAdded(food);
+        //
+        // _timer = 0;
 
     }
 
     public override void OnInteract()
     {
-        if(CurrentFood == null)
-        {
-            if(GameManager.Instance.HeldFoodItem != null)
-            {
-                _timer = 0;
-                AddFood(GameManager.Instance.RemoveFood());
-            }
-        }
-        else
-        {
-            if (GameManager.Instance.HeldFoodItem == null)
-            {
-                if (CurrentFood != null)
-                {
-                    GameManager.Instance.AddFood(RemoveFood());
-
-                    _timer = 0;
-                }
-            }
-        }
+        // if(CurrentFood == null)
+        // {
+        //     if(GameManager.Instance.HeldFoodItem != null)
+        //     {
+        //         _timer = 0;
+        //         AddFood(GameManager.Instance.RemoveFood());
+        //     }
+        // }
+        // else
+        // {
+        //     if (GameManager.Instance.HeldFoodItem == null)
+        //     {
+        //         if (CurrentFood != null)
+        //         {
+        //             GameManager.Instance.AddFood(RemoveFood());
+        //
+        //             _timer = 0;
+        //         }
+        //     }
+        // }
     }
 
     private void Update()
     {
-        if (CurrentFood != null)
-        {
-            _timer += Time.deltaTime;
-
-            if(_timer >= CurrentFood.CookTime)
-            {
-                var cookedFood = CurrentFood.Cooked;
-                RemoveFood();
-                AddFood(cookedFood);
-                _timer = 0;
-            }
-        }
+        // if (CurrentFood != null)
+        // {
+        //     _timer += Time.deltaTime;
+        //
+        //     if(_timer >= CurrentFood.CookTime)
+        //     {
+        //         var cookedFood = CurrentFood.Cooked;
+        //         RemoveFood();
+        //         AddFood(cookedFood);
+        //         _timer = 0;
+        //     }
+        // }
     }
 }
